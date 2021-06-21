da4fd5f9adffc9e50fec840910839a56fef70138
Damian Lis <foxprogramming93@gmail.com>
2021-06-21 12:24:44 +0200

    Created user details form fields in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,83 @@
+import React from 'react';
+import Header from 'components/Header';
+import { Form, Formik } from 'formik';
+import * as Yup from 'yup';
+import { makeStyles } from '@material-ui/core/styles';
+import { Container, Grid, Typography } from '@material-ui/core';
+import TextFieldWrapper from 'components/FormsUI/TextField';
+
+const useStyles = makeStyles((theme) => ({
+  formWrapper: {
+    marginTop: theme.spacing(5),
+    marginBottom: theme.spacing(8)
+  }
+}));
+
+const INITIAL_FORM_STATE = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  phone: ''
+};
+
+const FORM_VALIDATION = Yup.object().shape({
+  firstName: Yup.string().required('Required'),
+  lastName: Yup.string().required('Required'),
+  email: Yup.string().email('Invalid email.').required('Required'),
+  phone: Yup.number().integer().typeError('Please enter a valid phone number').required('Required')
+});
+
 function App() {
-  return <div> Hello world</div>;
+  const classes = useStyles();
+
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        <Header />
+      </Grid>
+      <Grid item xs={12}>
+        <Container maxWidth="md">
+          <div className={classes.formWrapper}>
+            <Formik
+              initialValues={{ ...INITIAL_FORM_STATE }}
+              validationSchema={FORM_VALIDATION}
+              onSubmit={(values) => console.log(values)}>
+              <Form>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <Typography>Your details</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <TextFieldWrapper name="firstName" label="First Name" />
+                  </Grid>
+
+                  <Grid item xs={6}>
+                    <TextFieldWrapper name="lastName" label="Last Name" />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <TextFieldWrapper name="email" label="Email" />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <TextFieldWrapper name="phone" label="Phone" />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <Typography>Address</Typography>
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <Typography>Booking information</Typography>
+                  </Grid>
+                </Grid>
+              </Form>
+            </Formik>
+          </div>
+        </Container>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default App;
