e404c6960a51702c980cc72fbb7d0b056b08d426
Damian Lis <foxprogramming93@gmail.com>
2021-06-21 12:45:45 +0200

    Created address details form fields in App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import * as Yup from 'yup';
 import { makeStyles } from '@material-ui/core/styles';
 import { Container, Grid, Typography } from '@material-ui/core';
 import TextFieldWrapper from 'components/FormsUI/TextField';
+import Select from 'components/FormsUI/Select';
+import countries from 'data/countries.json';
 
 const useStyles = makeStyles((theme) => ({
   formWrapper: {
@@ -17,14 +19,24 @@ const INITIAL_FORM_STATE = {
   firstName: '',
   lastName: '',
   email: '',
-  phone: ''
+  phone: '',
+  addressLine1: '',
+  addressLine2: '',
+  city: '',
+  state: '',
+  country: ''
 };
 
 const FORM_VALIDATION = Yup.object().shape({
   firstName: Yup.string().required('Required'),
   lastName: Yup.string().required('Required'),
   email: Yup.string().email('Invalid email.').required('Required'),
-  phone: Yup.number().integer().typeError('Please enter a valid phone number').required('Required')
+  phone: Yup.number().integer().typeError('Please enter a valid phone number').required('Required'),
+  addressLine1: Yup.string().required('Required'),
+  addressLine2: Yup.string(),
+  city: Yup.string().required('Required'),
+  state: Yup.string().required('Required'),
+  country: Yup.string().required('Required')
 });
 
 function App() {
@@ -65,6 +77,26 @@ function App() {
 
                   <Grid item xs={12}>
                     <Typography>Address</Typography>
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <TextFieldWrapper name="addressLine1" label="Address Line 1" />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <TextFieldWrapper name="addressLine2" label="Address Line 2" />
+                  </Grid>
+
+                  <Grid item xs={6}>
+                    <TextFieldWrapper name="city" label="City" />
+                  </Grid>
+
+                  <Grid item xs={6}>
+                    <TextFieldWrapper name="state" label="State" />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <Select name="country" label="Country" options={countries} />
                   </Grid>
 
                   <Grid item xs={12}>
